5f5a2e35c4d2d583ac122a4bb0706bf748c48f89
asalazarsgv <asalazarg@sgvsoftware.com>
2022-11-17 00:15:04 -0500

    Route

--- src/components/Productos/Computo/Cards.jsx
+++ src/components/Productos/Computo/Cards.jsx
@@ -14,7 +14,8 @@ import products from "../../../assets/productos.json";
 
 import TablatImg from "./TabletPC_64GB.jpg";
 
-export default function MediaCard() {
+export default function Cards({ producto }) {
+  console.log(producto);
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -26,24 +27,15 @@ export default function MediaCard() {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Tableta PC 64GB
+          {producto.nombre}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          1.Sistema operativo: android 6.0.cpu: mt6580, quad core. gpu:
-          mali-400. memoria de operación (ram): 2g ddr3. memoria corporal:
-          nano-flash 32g (puede utilizar menos de este valor, porque el espacio
-          de software móvil). 2.Expansión máxima de almacenamiento: tarjeta tf
-          64g. modo de red: gsm / tipo: quad core. 3.Tamaño de la memoria - rom:
-          64 gb rom (almacenamiento interno + almacenamiento de la tableta +
-          sistema utilizando algo de almacenamiento). 4.Modo de red: gsm / wcdma
-          / td-scdma. frecuencia de red: 900/1800/1900 ghz. 5.Número de tarjeta
-          sim: doble tarjeta de espera dual. tamaño de la tarjeta sim: ranura
-          estándar para tarjeta sim (25 mm × 15 mm × 0,8 mm).
+          {producto.descripcion}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Precio 5425</Button>
-        <Button size="small">SKU: 87659875</Button>
+        <Button size="small"> precio: {producto.precio} </Button>
+        <Button size="small"> sku: {producto.sku} </Button>
       </CardActions>
 
       <CardActions>
