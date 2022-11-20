eb753aedafadc88a13c35a0bb9f596e22ad2f01e
fernandosuarez8 <dsuarez53@estudiantes.areandina.edu.co>
2022-11-19 22:56:22 -0500

    Actualización Grid, Paper, Box

---
+++ src/components/Productos/Computo/Listarfer.jsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+import products from "../../../assets/productos.json";
+
+import Cards from "./Cards";
+
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+import TablatImg from "./TabletPC_64GB.jpg";
+
+export default function Listarfer() {
+  function renderCards() {
+    return products.map((producto) => {
+      return <Cards producto={producto} />;
+    });
+  }
+
+  return <div>{renderCards()}</div>;
+}
+
+/* import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function RowAndColumnSpacing() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <Item>1</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>2</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>3</Item>
+        </Grid>
+        <Grid item xs={6}>
+          <Item>4</Item>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+} */
